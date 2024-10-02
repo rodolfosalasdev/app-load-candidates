b773b98b20a6afe62fd9c3774667388dd0761d84
Rodolfo Salas de Lima <rodolfosalas@Rodolfos-MacBook-Pro.local>
2024-10-02 09:02:57 -0300

    test: Pipe, availability

--- src/app/_pipes/availability-pipe.pipe.spec.ts
+++ src/app/_pipes/availability-pipe.pipe.spec.ts
@@ -1,8 +1,27 @@
+import { PipeTransform } from '@angular/core';
 import { AvailabilityPipe } from './availability-pipe.pipe';
 
 describe('AvailabilityPipePipe', () => {
+  let pipe: AvailabilityPipe;
+
+  beforeEach(() => {
+    pipe = new AvailabilityPipe();
+  });
+  
   it('create an instance', () => {
-    const pipe = new AvailabilityPipe();
     expect(pipe).toBeTruthy();
+  })
+  
+  it('should turn `true` into "Yes"', () => {
+    expect(pipe.transform(true)).toBe('Yes');
+  });
+
+  it('should turn `false` into "No"', () => {
+    expect(pipe.transform(false)).toBe('No');
+  });
+
+  it('should return the original value when it is not boolean', () => {
+    expect(pipe.transform('other')).toBe('other');
+    expect(pipe.transform(123)).toBe(123);
   });
 });
